25793a6392e7b10e2ab117a4db958859c26445a4
RozaBaitikova <104209160+RozaBaitikova@users.noreply.github.com>
2022-04-25 17:53:09 +0600

    initial commit

---
+++ bin/lesson_variables.dart
@@ -0,0 +1,47 @@
+void main(List<String> arguments) {
+  print('First Home Work!');
+int a = 3; 
+double b = 4.20; 
+print(a+b); 
+String text = 'Name'; 
+text = 'flutter'; 
+print ('I want to learn $text'); 
+
+double myNumber = 2.225; 
+String myText; 
+myText = myNumber.toString();
+print(myText);
+
+
+ String address = 'Suumbaeva st., Bishkek';
+ var count = address.length; 
+ print (count); 
+
+ int age = 20; 
+ var r = age.toString(); 
+double weight = 63.20;  
+var p = weight.toString(); 
+String name = 'Daniyar';  
+String total = ('возраст:' + r + ', ' + 'вес:'+ p + ', ' + 'имя:' + name);   
+print (total); 
+
+int z = 20; 
+String x = '241292'; 
+int c = int.parse(x);  
+z = c; 
+
+print (z); 
+
+
+
+  
+
+
+    
+
+
+
+
+
+
+}
